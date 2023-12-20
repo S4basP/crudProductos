cf7ecfbd9799d87090428e3e61743a320ea18861
S4basP <sabas.pineda.merino@gmail.com>
2023-12-19 19:23:37 -0600

    Se agrego actualizar y eliminar producto

--- src/controllers/consultas.js
+++ src/controllers/consultas.js
@@ -23,23 +23,33 @@ export const subirDatos = async (req, res) => {
   }
 };
 
-export const actualizarProducto = ( req, res ) =>
-{
-  
-}
+export const actualizarProducto = async (req, res) => {
+  const { id, nombre, unidades, precio_Por_Unidad } = req.body;
+  try {
+    const result = await conn.query(
+      "update productos set nombre = ifnull( ?, nombre ), unidades = ifnull( ?, unidades ) precio_Por_Unidad = ifnull ( ?, precio_Por_Unidad )  where id = ?; ",
+      [nombre, unidades, precio_Por_Unidad, id]
+    );
 
+    if (result.affectedRows === 0)
+      return res.status(404).json({
+        message: "Product not update ",
+      });
+  } catch {
+    res.status(505).json({
+      message: "Somethin goes wrong"
+    });
+  }
+};
 
 export const eliminarProducto = async (req, res) => {
   const { id } = req.body;
   try {
-
     const [rows] = await conn.query("delte from product where id = ?", [id]);
     if (result.affectedRows <= 0)
-      return res.status(404).joson({ message: "Employee not found" });
+      return res.status(404).joson({ message: "Product not found" });
     res.sendStatus(204);
-
   } catch {
-
     return res.status(500).json({
       message: "Somethin goes wrong",
     });
